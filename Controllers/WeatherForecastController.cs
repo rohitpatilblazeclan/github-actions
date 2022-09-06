2f90e95573eda4094c1b33a23dbb862bb6710920
rohitpatil1131996 <rohit.patil@intellihub.com.au>
2022-09-06 13:08:02 +0530

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAPIApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("forecastdata")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet("employeeData")]
+        public string GetEmployee()
+        {
+            return "emp1,emp2";
+        }
+
+        [HttpGet("studentData")]
+        public string GetStudent()
+        {
+            return "student1,student2";
+        }
+        [HttpGet("falseData2")]
+        public string Getdata()
+        {
+            return "student1,student2";
+        }
+        [HttpGet("falseData3")]
+        public string Getdata1()
+        {
+            return "student1,student2";
+        }
+    }
+}
